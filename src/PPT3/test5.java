d88a9fd6d40f0f5d7824495f055c46a2562a1e75
TTLYB <2970073658@qq.com>
2025-06-09 18:55:29 +0800

    Add files via upload

---
+++ src/PPT3/test5.java
@@ -0,0 +1,26 @@
+package PPT3;
+//文钱可以买一只公鸡，3文钱可以买一只母鸡，1文钱可以买3只雏鸡。现在用100文钱买100只鸡
+public class test5 {
+    public static boolean chickenSum(int x,int y,int z) {
+        if (5 * x + y * 3 + (z-z%3) / 3 == 100 && x + y + z == 100) {
+            return true;
+            //  System.out.println(x + "只公鸡" + y + "只母鸡" + z + "只小鸡");
+        } else
+            return false;
+    }
+
+    public static void main(String[] args) {
+        for (int a = 1; a < 20; a++) {
+            for (int b = 1; b < 30; b++) {
+                for (int c = 3; c < 100; c=c+3) {
+
+                    if(chickenSum(a, b, c)) {
+                        System.out.println(a + "只公鸡" + b + "只母鸡" + c + "只小鸡");
+                    }
+                }
+            }
+        }
+
+    }
+}
+
